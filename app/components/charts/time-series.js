ad2be9eaf7ea2a2af31b516919860874985b257b
cloke <cory@me.com>
2022-06-18 18:13:46 -0400

    v2

--- app/components/charts/time-series.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-apache-echarts/components/charts/time-series.js';
--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  name: require('./package').name,
-  isDevelopingAddon: function () {
-    return true;
-  },
-};

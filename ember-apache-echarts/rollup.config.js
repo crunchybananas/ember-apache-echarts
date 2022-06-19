75e5f32caa7dad196c716f3f0bec368f058c6066
cloke <cory@me.com>
2022-06-19 19:38:41 -0400

    Add lodash

--- ember-apache-echarts/rollup.config.js
+++ ember-apache-echarts/rollup.config.js
@@ -1,13 +1,12 @@
 import babel from '@rollup/plugin-babel';
 import { Addon } from '@embroider/addon-dev/rollup';
-import { defineConfig } from 'rollup';
 
 const addon = new Addon({
   srcDir: 'src',
   destDir: 'dist',
 });
 
-export default defineConfig({
+export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
   // You can augment this if you need to.
   output: addon.output(),
@@ -17,36 +16,30 @@ export default defineConfig({
     // addon. Anything not listed here may get optimized away.
     addon.publicEntrypoints([
       'components/**/*.js',
+      'services/**/*.js',
       'modifiers/**/*.js'
-      ]),
+    ]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
     addon.appReexports([
       'components/**/*.js',
-    //   'helpers/**/*.js',
-      'modifiers/**/*.js',
-    //   'services/**/*.js',
-    //   'initializers/**/*.js',
-    //   'instance-initializers/**/*.js',
-    //   'utils/**/*.js',
+      'services/**/*.js',
+      'utils/**/*.js',
+      'modifiers/**/*.js'
     ]),
-
-    // This babel config should *not* apply presets or compile away ES modules.
-    // It exists only to provide development niceties for you, like automatic
-    // template colocation.
-    //
-    // By default, this will load the actual babel config from the file
-    // babel.config.json.
-    babel({
-      babelHelpers: 'bundled',
-    }),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
     // package names.
     addon.dependencies(),
+
+    // This babel config should *not* apply presets or compile away ES modules.
+    // It exists only to provide development niceties for you, like automatic
+    // template colocation.
+    // See `babel.config.json` for the actual Babel configuration!
+    babel({ babelHelpers: 'bundled' }),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
@@ -58,4 +51,4 @@ export default defineConfig({
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
   ],
-});
+};

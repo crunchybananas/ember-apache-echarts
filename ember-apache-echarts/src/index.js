dc29bbb5e1e1abefdab3689a2395ca9fbe5f4d6f
cloke <cory@me.com>
2022-06-19 17:26:10 -0400

    Update package

--- ember-apache-echarts/src/index.js
+++ ember-apache-echarts/src/index.js
@@ -1,4 +1,5 @@
-// export * as TimeSeries from './components/chart/time-series';
-// export * as PieChart from './components/chart/pie';
+export * from './components/chart/pie';
+export * as TimeSeries from './components/chart/time-series';
+export * as PieChart from './components/chart/pie';
 
 export * from './utils/merge-at';

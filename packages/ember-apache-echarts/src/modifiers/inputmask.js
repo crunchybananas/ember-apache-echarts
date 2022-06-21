aaf789987878fcc1bf5efdbc146673cd7c2e0f3d
Trevor Lohrbeer <trevor@fastfedora.com>
2022-06-21 14:32:02 +0200

    clean: Remove unused code

--- packages/ember-apache-echarts/src/modifiers/inputmask.js
+++
@@ -1,6 +0,0 @@
-import { modifier } from 'ember-modifier';
-import Inputmask from 'inputmask';
-
-export default modifier(function inputmask(element, params, hash) {
-  new Inputmask(hash).mask(element);
-});

2b58473825582dd429c5b0ddee9322ab7280b684
Trevor Lohrbeer <trevor@fastfedora.com>
2022-07-20 15:29:08 +0200

    feat: Add bar chart component

---
+++ packages/ember-apache-echarts/src/components/chart/bar.js
@@ -0,0 +1,58 @@
+import pick from 'lodash/pick';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+/**
+ * Converts an EChart tooltip param into a standardized & simplied object this
+ * component can yield to custom tooltip components.
+ *
+ * The object returned from this function includes the following properties:
+ *
+ * `label`
+ * : The label used for the bar.
+ *
+ * `value`
+ * : The value used for the bar.
+ *
+ * `marker`
+ * : Raw HTML that renders the marker used to identify the item on the chart.
+ *
+ * `dataIndex`
+ * : The index of the item in the data for the series this item belongs to.
+ *
+ * `bar`
+ * : An object representing the graphical properties of the bar: `color` and
+ *   `percent` of circle.
+ */
+const toTooltipItem = (param) => ({
+  ...pick(param, 'value', 'marker', 'dataIndex'),
+  label: param.name,
+  bar: pick(param, 'color'),
+
+  // TODO: Think about how/if we need to support multiple series for bar charts.
+  //       [twl 30.Apr.22]
+  //
+  // series: {
+  //   ...dataset[param.seriesIndex],
+  //   index: param.seriesIndex,
+  // },
+});
+
+export default class BarChartComponent extends Component {
+  itemTooltipElement;
+
+  @tracked tooltipItem;
+
+  @action
+  setup(element) {
+    this.itemTooltipElement = element.querySelector('[data-role=itemTooltip]');
+  }
+
+  @action
+  tooltipFormatter(params) {
+    this.tooltipItem = toTooltipItem(params);
+
+    return this.itemTooltipElement;
+  }
+}

e51879a1ca9d91c60b8d96e6521e3f0bdc5ee622
cloke <cory@me.com>
2022-08-23 16:01:29 -0400

    Lint

--- packages/test-app/app/components/chart-pie-example.js
+++ packages/test-app/app/components/chart-pie-example.js
@@ -1,15 +1,18 @@
 import Component from '@glimmer/component';
 
 export default class ChartPieExample extends Component {
-  chartData =      [{
+  chartData = [
+    {
       name: 'sent',
       value: 2,
-    }, {
+    },
+    {
       name: 'draft',
       value: 1,
-    }, {
+    },
+    {
       name: 'rejected',
       value: 13,
-    }]
-  ;
+    },
+  ];
 }

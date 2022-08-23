48b4c2c32f00eea5ff40103bbf499374441544af
cloke <cory@me.com>
2022-08-23 16:15:48 -0400

    Add root url

--- packages/test-app/config/environment.js
+++ packages/test-app/config/environment.js
@@ -44,6 +44,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/ember-apache-echarts';
     // here you can enable a production-specific feature
   }
 

1685d0083fd22fffd3cba42648ec368bc47e4592
cloke <cory@me.com>
2022-08-30 16:48:00 -0400

    Add support for a single line chart

---
+++ packages/test-app/app/components/chart-line-example.js
@@ -0,0 +1,30 @@
+import Component from '@glimmer/component';
+
+export default class ChartLineExample extends Component {
+  chartData = [
+    {
+      x: '2020-01-01',
+      y: 6126,
+    },
+    {
+      x: '2020-02-01',
+      y: 100.88,
+    },
+    {
+      x: '2020-03-01',
+      y: 4953,
+    },
+    {
+      x: '2020-04-01',
+      y: 4678,
+    },
+    {
+      x: '2020-05-01',
+      y: 4321,
+    },
+    {
+      x: '2020-06-01',
+      y: 3952,
+    },
+  ];
+}

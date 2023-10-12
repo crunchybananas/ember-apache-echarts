7dc0b8cbcec47a2ef40d4c6cdcbe0005ef736386
cloke <cory@me.com>
2023-10-12 10:55:53 -0400

    Update prettier config

--- ember-apache-echarts/.prettierrc.js
+++ ember-apache-echarts/.prettierrc.js
@@ -4,6 +4,14 @@ module.exports = {
   singleQuote: true,
   overrides: [
     {
+      // Lol, JavaScript
+      files: ['*.js', '*.ts', '*.cjs', '.mjs', '.cts', '.mts', '.cts'],
+      options: {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    },
+    {
       files: '*.hbs',
       options: {
         singleQuote: false,
